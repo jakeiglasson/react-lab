77a41ff6bf78cb37e5b0f61b7e5991b5e3d95551
Jake Glasson <jake.glasson@lotterywest.wa.gov.au>
2022-12-22 14:53:38 +0800

    add airbnb styling to prettier config
    add prettier defaults in comment

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,28 +1,40 @@
 module.exports = {
-  // semi: true,
-  trailingComma: 'all',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  semi: true,
   singleQuote: true,
-  // printWidth: 90,
-  // tabWidth: 2,
-  // endOfLine: "auto",
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
 };
 
 // {
-//   "arrowParens": "always",
-//   "bracketSpacing": true,
-//   "embeddedLanguageFormatting": "auto",
-//   "htmlWhitespaceSensitivity": "css",
-//   "insertPragma": false,
-//   "jsxBracketSameLine": false,
-//   "jsxSingleQuote": false,
-//   "proseWrap": "preserve",
-//   "quoteProps": "as-needed",
-//   "requirePragma": false,
-//   "semi": true,
-//   "singleQuote": false,
-//   "tabWidth": 2,
-//   "trailingComma": "es5",
-//   "useTabs": false,
-//   "vueIndentScriptAndStyle": false,
-//   "printWidth": 100
+//   arrowParens: 'always';
+//   bracketSameLine: false;
+//   bracketSpacing: true;
+//   embeddedLanguageFormatting: 'auto';
+//   endOfLine: 'lf';
+//   filepath: undefined;
+//   htmlWhitespaceSensitivity: 'css';
+//   insertPragma: false;
+//   jsxBracketSameLine: false;
+//   jsxSingleQuote: false;
+//   parser: undefined;
+//   printWidth: 80;
+//   proseWrap: 'preserve';
+//   quoteProps: 'as-needed';
+//   rangeEnd: Infinity;
+//   requirePragma: false;
+//   semi: true;
+//   singleAttributePerLine: false;
+//   singleQuote: false;
+//   tabWidth: 2;
+//   trailingComma: 'es5';
+//   useTabs: false;
+//   vueIndentScriptAndStyle: false;
 // }
